ee8a5fe59f3ff0ae12f822a80b709339f3d7b259
Théo Barreto Silva <barretotheo25@gmail.com>
2022-07-08 08:09:03 -0300

    Add files via upload

---
+++ EstoCar/src/estocar/EstoCar.java
@@ -0,0 +1,12 @@
+package estocar;
+
+import JFrame.TelaInicial;
+
+public class EstoCar {
+
+    public static void main(String[] args) {
+        TelaInicial tela = new TelaInicial();
+        tela.setVisible(true);
+    }
+    
+}
