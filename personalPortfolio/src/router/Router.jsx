54ae368e646b62cb7419ca965c56f604a5a3056a
Théo Barreto Silva <barretotheo25@gmail.com>
2023-02-16 18:14:22 -0300

    fazendo tela inicial

---
+++ personalPortfolio/src/router/Router.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { TelaInicio } from "../pages/TelaInicio/TelaInicio"
+import { TelaProjetos } from "../pages/TelaProjetos/TelaProjetos"
+
+export const Router = () => {
+    return(
+        <Routes>
+            <Route exact path="/" element={<TelaInicio />} />
+            <Route exact path="/projetos" element={<TelaProjetos />} />
+        </Routes>
+    )
+}
