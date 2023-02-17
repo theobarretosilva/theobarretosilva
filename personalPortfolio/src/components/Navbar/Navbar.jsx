99934985d1a023434795bd1161ee8a1dc0e37afd
Théo Barreto Silva <barretotheo25@gmail.com>
2023-02-17 12:29:38 -0300

    upload

--- personalPortfolio/src/components/Navbar/Navbar.jsx
+++ personalPortfolio/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import { HeaderStyled } from "./Navbar.styled"
+import { DivNavStyled, HeaderStyled, NomeTheoStyled, PNavStyled } from "./Navbar.styled"
 
 export const Navbar = () => {
     return(
         <HeaderStyled>
-            
+            <NomeTheoStyled>Théo Silva</NomeTheoStyled>
+            <DivNavStyled>
+                <PNavStyled>Início</PNavStyled>
+                <PNavStyled>Projetos</PNavStyled>
+            </DivNavStyled>
         </HeaderStyled>
     )
 }
--- personalPortfolio/src/components/Navbar/Navbar.styled.jsx
+++ personalPortfolio/src/components/Navbar/Navbar.styled.jsx
@@ -5,4 +5,25 @@ export const HeaderStyled = styled.header`
     top: 0;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    padding: 1vw 3vw;
+`
+
+export const NomeTheoStyled = styled.p`
+    font-family: 'Nothing You Could Do', cursive;
+    font-size: 3vw;
+    color: white;
+    margin: 0;
+`
+
+export const DivNavStyled = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const PNavStyled = styled.p`
+    font-family: 'Arbutus Slab', sans-serif;
+    color: white;
+    
 `
