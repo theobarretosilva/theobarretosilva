118b73ac2c27b864b3536cdc3711e8368346cb48
Théo Barreto Silva <barretotheo25@gmail.com>
2023-06-29 11:31:14 -0300

    fazendo Tecnologias

--- personalPortfolio/src/components/sections/Tecnologias/Tecnologias.styles.ts
+++ personalPortfolio/src/components/sections/Tecnologias/Tecnologias.styles.ts
@@ -1,13 +1,48 @@
 import styled from "styled-components";
 
-export const DivBarraInicio = styled.div`
+export const SectionTec = styled.section`
+    padding-bottom: 5vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const DivTecs = styled.div`
+    padding-top: 1vh;
+    display: flex;
+    align-items: center;
+`;
+
+export const BtnNext = styled.button`
+    border: none;
+    width: 2.5rem;
+    height: 2.5rem;
+    z-index: 1;
+    background: none;
+    cursor: pointer;
+    padding-bottom: 0;
+    padding-top: 0;
+`;
+
+export const Carousel = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
-    padding-bottom: 1.5vw;
+    align-items: center;
+    gap: 5vw;
+    width: 50rem;
+    overflow-x: auto;
+    scroll-behavior: smooth;
+    border-radius: 8px;
+    background: #493353;
+    padding: 3vh 1vw 3vh 1vw;
+    margin: 0;
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
 `
 
 export const ImgHabInicio = styled.img`
-    width: 3.5vw;
-    height: 3.5vw;
+    width: 4vw;
+    height: 4vw;
 `
--- personalPortfolio/src/components/sections/Tecnologias/Tecnologias.tsx
+++ personalPortfolio/src/components/sections/Tecnologias/Tecnologias.tsx
@@ -1,26 +1,47 @@
 import { H1TituloSecao } from "../../PTituloSecao/PTituloSecao"
-import { DivBarraInicio, ImgHabInicio } from "./Tecnologias.styles"
-import React from "react"
+import * as S from "./Tecnologias.styles"
+import React, { useRef } from "react"
 
 export const Tecnologias = () => {
+    const carousel = useRef(null)
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current?.offsetWidth
+    }
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    }
+
     return(
-        <section id="tecnologias">
+        <S.SectionTec id="tecnologias">
             <H1TituloSecao>TECNOLOGIAS</H1TituloSecao>
-            <DivBarraInicio>
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Java" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="TypeScript" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" alt="AndroidStudio" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg" alt="PostgreSQL" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" alt="Firebase" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="MongoDB" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg" alt="NestJS" />
-                <ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" alt="Aws" />
-            </DivBarraInicio>
-        </section>
+            <S.DivTecs>
+                <S.BtnNext onClick={handleLeftClick}><img src="../../../src/assets/svgs/arrow-right.svg" /></S.BtnNext>
+                <S.Carousel ref={carousel}>
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Java" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="TypeScript" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg" alt="PostgreSQL" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" alt="AndroidStudio" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" alt="Firebase" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg" alt="NestJS" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="MongoDB" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" alt="Aws" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="NodeJS" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt="Figma" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" alt="Trello" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg" alt="Canva" />
+                    <S.ImgHabInicio src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg" alt="Slack" />
+                </S.Carousel>
+                <S.BtnNext onClick={handleRightClick} style={{ rotate: '180deg' }}><img src="../../../src/assets/svgs/arrow-right.svg" /></S.BtnNext>
+            </S.DivTecs>
+        </S.SectionTec>
     )
 }
