95887e0642cca371ab9b9acd57a866f739fd8b13
Théo Barreto Silva <barretotheo25@gmail.com>
2024-05-06 15:14:51 -0300

    upload foto e alteração de nome de pasta

--- personalPortfolio/src/pages/TelaInicio/TelaInicio.styled.ts
+++ personalPortfolio/src/pages/TelaInicio/TelaInicio.styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SectionInicioStyled = styled.main`
-    background-color: #0A000E;
+    background-color: #190023;
     top: 0;
     right: 0;
     left: 0;
--- personalPortfolio/src/pages/TelaInicio/TelaInicio.tsx
+++ personalPortfolio/src/pages/TelaInicio/TelaInicio.tsx
@@ -1,9 +1,9 @@
-import { Tecnologias } from "../../components/sections/Tecnologias/Tecnologias"
-import { SobreMim } from "../../components/sections/SobreMim/SobreMim"
+import { Tecnologias } from "../../components/Secoes/Tecnologias/Tecnologias"
+import { SobreMim } from "../../components/Secoes/SobreMim/SobreMim"
 import { Lenguage } from "../../components/Lenguage/Lenguage"
 import { Navbar } from "../../components/Navbar/Navbar"
 import { Rodape } from "../../components/Rodape/Rodape"
-import { Servicos } from "../../components/sections/Servicos/Servicos"
+import { Servicos } from "../../components/Secoes/Servicos/Servicos"
 import { SectionInicioStyled } from "./TelaInicio.styled"
 import React from "react"
 
